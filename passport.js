2b7b3ef228bc0fac87a813c7deb39e1ba053b0f4
devprdhana <dhana@wunderux.com>
2024-01-09 16:28:25 +0530

    Your commit message here

--- passport.js
+++ passport.js
@@ -1,23 +1,47 @@
+// const passport =require("passport")
+// const GoogleStrategy = require('passport-google-oauth2').Strategy;
+// const dotenv = require('dotenv')
+// dotenv.config()
+
+// passport.serializeUser(function(user, done) {
+//     done(null, user);
+// });
+
+// passport.deserializeUser(function(user, done) {
+//         done(null, user);
+// });
+
+// passport.use(new GoogleStrategy({
+//         clientID:process.env.GOOGLE_CLIENT_ID,
+//         clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+//         callbackURL: "http://localhost:5000/google/callback",
+//         passReqToCallback   : true
+//     },
+//     function(request, accessToken, refreshToken, profile, done) {
+//             return done(null, profile);
+//     }
+// ));
+
+require('dotenv').config()
 const passport =require("passport")
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
-const dotenv = require('dotenv')
-dotenv.config()
+
+
+passport.use(new GoogleStrategy({
+        clientID: process.env.clientID,
+        clientSecret: process.env.clientSecret,
+        callbackURL: "http://localhost:5000/google/callback",
+        passReqToCallback   : true
+},
+function(request, accessToken, refreshToken, profile, done) {
+        return done(null, profile);
+}
+));
 
 passport.serializeUser(function(user, done) {
-    done(null, user);
+done(null, user);
 });
 
 passport.deserializeUser(function(user, done) {
-        done(null, user);
+done(null, user);
 });
-
-passport.use(new GoogleStrategy({
-        clientID:process.env.GOOGLE_CLIENT_ID,
-        clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:5000/google/callback",
-        passReqToCallback   : true
-    },
-    function(request, accessToken, refreshToken, profile, done) {
-            return done(null, profile);
-    }
-));
