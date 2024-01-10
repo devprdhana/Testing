6229adba2ba642a5b4fbdccd71c18a20fe9ae4f0
devprdhana <dhana@wunderux.com>
2024-01-10 15:10:04 +0530

    job-routes and controller updated

--- controllers/job-controller.js
+++ controllers/job-controller.js
@@ -5,10 +5,12 @@ const postJob = async (req, res, next) => {
     jobTitle,
     jobDescription,
     paymentOneTime,
+    requiredSkills,
     paymentOngoing,
     oneTime,
     onGoing,
-    appliedUsers
+    appliedUsers,
+    client
   } = req.body;
   let job;
   try {
@@ -17,9 +19,11 @@ const postJob = async (req, res, next) => {
       jobDescription,
       paymentOneTime,
       paymentOngoing,
+      requiredSkills,
       oneTime,
       onGoing,
-      appliedUsers
+      appliedUsers,
+      client
     });
     job = await job.save()
   } catch (err) {
@@ -33,5 +37,56 @@ const postJob = async (req, res, next) => {
   res.status(201).json(job)
 };
 
+const getJobById = async(req,res,next)=>{
+  const {id} = req.params
+  console.log(id)
+  let job ;
+  try{
+    job = await Job.findById(id)
+  }catch(err){
+    console.log(err)
+  }
+
+  if(job){
+    res.status(200).json(job)
+
+  }else {
+    res.status(404).json({message:"Job not found"})
+  }
+
+}
+
+const getJobBySkill = async(req,res,next)=>{
+  const {skills} = req.body
+  console.log(skills)
+  let jobs;
+  try{
+    jobs = await Job.find()
+  }catch(err){
+    console.log(err)
+  }
+  console.log("jobs",jobs)
+  if(!jobs){
+    res.status(500).json({message:"Internal sever error"})
+  }
+
+  const filterJobsBySkills = (jobs, skills) => {
+    return jobs.filter(job => {
+      const jobSkills = job.requiredSkills || [];
+      // Check if any of the required skills match with the skills array
+      return skills.some(skill => jobSkills.includes(skill));
+    });
+  };
+  
+  const filteredJobs = filterJobsBySkills(jobs, skills);
+  
+  console.log(filteredJobs);
+  
+
+  res.status(200).json(filteredJobs)
+} 
+
 
 exports.postJob = postJob
+exports.getJobById = getJobById
+exports.getJobBySkill = getJobBySkill
